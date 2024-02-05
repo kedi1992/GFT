7e32251927129669cbb2b853a0dc449b48d04fc5
Pravin Kedar <kedi1992@gmail.com>
2024-02-05 11:44:52 +0530

    add APITest Case

--- services/api/tests.py
+++ services/api/tests.py
@@ -1,5 +1,10 @@
+import json
+
 from django.test import TestCase
 from api.serializers import UserSerializer
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from django.contrib.auth import get_user_model
 
 
 class UserSerializerTest(TestCase):
@@ -11,3 +16,25 @@ class UserSerializerTest(TestCase):
         serializer = UserSerializer(data={"password": "ddaasa","first_name": "John","last_name": "Doe","date_of_birth": "1990-01-01","phone_number": "1234567890"})
         self.assertFalse(serializer.is_valid())
 
+
+class UserAPITest(APITestCase):
+    def setUp(self):
+        self.data = {"username": "testuser","email": "user123@example.com","password": "ddaasa","first_name": "John","last_name": "Doe"}
+        self.user = get_user_model().objects.create_user(**self.data)
+
+    def test_create_user(self):
+        api_url = reverse("register")
+        payload_data = {"username": "abc887","email": "user123@example.com","password": "ddaasa","first_name": "John","last_name": "Doe","date_of_birth": "1990-01-01","phone_number": "1234567890"}
+        res = self.client.post(api_url, payload_data)
+        self.assertEqual(res.status_code, 201)
+
+    def test_get_token(self):
+        token_url = reverse("token_obtain_pair")
+        payload_data = {"username": self.data['username'], "password": self.data['password']}
+        res = self.client.post(token_url, payload_data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(list(json.loads(res.content.decode('utf-8')).keys()), ['token'])
+
+    #
+    # def test_user_logout(self):
+    #     pass
